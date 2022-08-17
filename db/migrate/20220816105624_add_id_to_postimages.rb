5ea597e769d5cfd8688d617e992296a9f0d7927e
EC2 Default User <ec2-user@ip-172-31-28-38.ec2.internal>
2022-08-17 06:17:36 +0000

    [Finish] 4-11

--- db/migrate/20220816105624_add_id_to_postimages.rb
+++
@@ -1,7 +0,0 @@
-class AddIdToPostimages < ActiveRecord::Migration[6.1]
-  def change
-    add_column :postimages, :shop_name, :string
-    add_column :postimages, :caption, :text
-    add_column :postimages, :user_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_16_045854) do
+ActiveRecord::Schema.define(version: 2022_08_17_052201) do
 
   create_table "post_images", force: :cascade do |t|
+    t.string "shop_name"
+    t.text "caption"
+    t.integer "user_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
